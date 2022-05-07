76105ff2dbb4be1269d21dfdb322da01871e9464
Bartosz <228805@edu.p.lodz.pl>
2022-05-07 22:17:17 +0200

    added tests

--- src/test/java/edu/iis/mto/testreactor/doser/MedicineDoserTest.java
+++ src/test/java/edu/iis/mto/testreactor/doser/MedicineDoserTest.java
@@ -1,13 +1,16 @@
 package edu.iis.mto.testreactor.doser;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 import edu.iis.mto.testreactor.doser.infuser.Infuser;
+import edu.iis.mto.testreactor.doser.infuser.InfuserException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
-import org.mockito.internal.stubbing.answers.DoesNothing;
+import org.mockito.Mockito;
+import org.mockito.internal.stubbing.*;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.concurrent.TimeUnit;
@@ -20,25 +23,57 @@ class MedicineDoserTest {
     private Clock clock;
     @Mock
     private DosageLog dosageLog;
+
     private MedicineDoser medicineDoser;
+    private Dose dose;
+    private Receipe receipe ;
+    private Medicine medicine;
+
 
     @BeforeEach
-    void setUp() throws Exception{
+    void setUp(){
         medicineDoser=new MedicineDoser(infuser,dosageLog,clock);
-    }
-
-    @Test
-    void medicineDoserShouldHaveOne() {
-        Medicine medicine = Medicine.of("Aspirin");
+        medicine = Medicine.of("Aspirin");
         CapacityUnit capacityUnit = CapacityUnit.MILILITER;
         Capacity capacity=Capacity.of(2,capacityUnit);
         TimeUnit timeUnit=TimeUnit.DAYS;
         Period period = Period.of(14,timeUnit);
-        Dose dose = Dose.of(capacity,period);
-        Receipe receipe = Receipe.of(medicine,dose,1);
-        MedicinePackage medicinePackage = MedicinePackage.of(medicine,capacity);
+        dose = Dose.of(capacity,period);
+        receipe = Receipe.of(medicine,dose,1);
+        MedicinePackage medicinePackage = MedicinePackage.of(medicine, capacity);
         medicineDoser.add(medicinePackage);
+    }
+
+    @Test
+    void medicineDoserShouldReturnDoseWithSuccess() {
         DosingResult result = medicineDoser.dose(receipe);
         assertEquals(DosingResult.SUCCESS, result);
+    }
+
+    @Test
+    void medicineDoserInDoseMethodShouldReturnUnavailableMedicineException() throws InfuserException {
+        Medicine medicine = Medicine.of("NULL");
+        receipe = Receipe.of(medicine,dose,1);
+        assertThrows(UnavailableMedicineException.class,()->medicineDoser.dose(receipe));
+    }
+
+    @Test
+    void medicineDoserInDoseMethodShouldReturnInsufficientMedicineException(){
+        receipe = Receipe.of(medicine,dose,3);
+        assertThrows(InsufficientMedicineException.class,()->medicineDoser.dose(receipe));
+    }
+
+    @Test
+    void medicineDoserInDoseMethodShouldCallLogDifuserErrorWhenItReturnException() throws InfuserException {
+        doThrow(InfuserException.class).when(infuser).dispense(any(MedicinePackage.class),any(Capacity.class));
+        medicineDoser.dose(receipe);
+        verify(dosageLog,times(1)).logDifuserError(any(),any());
+    }
+
+    @Test
+    void medicineDoserInDoseMethodShouldCallLogStartandLogEndTheSameNumberOfTimes(){
+        medicineDoser.dose(receipe);
+        verify(dosageLog,times(1)).logStart();
+        verify(dosageLog,times(1)).logEnd();
     }
 }
